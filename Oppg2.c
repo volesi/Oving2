c16d9867516675ca3f97f2c0a4cead25e9b922f4
volesi <bragege@itk-d651>
2015-01-23 12:49:34 +0100

    Ferdig C versjon

--- Oppg2.c
+++ Oppg2.c
@@ -1,33 +1,48 @@
+//gcc -std=gnu99 -Wall -g -o Oppg2 Oppg2.c -lpthread
+
 #include <pthread.h>
 #include <stdio.h>
 
+
 int i = 0;
-int N = 1000000;
+int N = 100;
+
 
 // Note the return type: void*
-void* Inkrementer(){
-    for (int j = 0; j < N; j++)
-    	i++;
+void* Inkrementer(void* lock){
+    for (int j = 0; j < N; j++){
+	pthread_mutex_lock(lock);
+    	i+=2;
+	pthread_mutex_unlock(lock);
+    }
     return NULL;
 }
 
-void* Dekrementer(){
-    for (int j = 0; j < N; j++)
+void* Dekrementer(void* lock){
+    for (int j = 0; j < N; j++){
+    	pthread_mutex_lock(lock);
     	i--;
+	pthread_mutex_unlock(lock);
+    }
     return NULL;
 }
 
 
 
 int main(){
+    pthread_mutex_t lock;
+    pthread_mutex_init(&lock,NULL);
+    
     pthread_t someThread;
     pthread_t someThread2;
-    pthread_create(&someThread, NULL, Inkrementer, NULL);
-    pthread_create(&someThread2, NULL, Dekrementer, NULL);
+    pthread_create(&someThread, NULL, Inkrementer,(void*) &lock);
+    pthread_create(&someThread2, NULL, Dekrementer,(void*) &lock);
     
     pthread_join(someThread, NULL);
     pthread_join(someThread2, NULL);
     printf("%d\n", i);
+
+    pthread_mutex_destroy(&lock);
     return 0;
     
 }
