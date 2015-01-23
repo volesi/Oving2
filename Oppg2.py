882fe982c2b4540de77fc31190d6bf9d35fb6f9a
volesi <bragege@itk-d651>
2015-01-23 12:12:46 +0100

    Fikset Låsene

--- Oppg2.py
+++ Oppg2.py
@@ -1,7 +1,7 @@
 #Ny pythonfil
 #python Oppg2.py
 
-from threading import Thread
+from threading import Thread, Lock
 
 times = 500;
 i = 0
@@ -10,7 +10,7 @@ def Inkrementer(lock):
   global i
   for _ in range(times):
     lock.acquire()
-    i += 1
+    i += 2
     lock.release()
   
 def Dekrementer(lock):
@@ -22,8 +22,8 @@ def Dekrementer(lock):
   
 def main():
   lock=Lock()
-  someThread = Thread(target = Inkrementer, args = (lock),)
-  someThread2 = Thread(target = Dekrementer, args = (lock),)
+  someThread = Thread(target = Inkrementer, args = (lock,),)
+  someThread2 = Thread(target = Dekrementer, args = (lock,),)
   someThread.start()
   someThread2.start()
   someThread.join()
