580c74601039d0f6f05c65bec341f8e90b8b3079
volesi <brage.g.e@gmail.com>
2015-01-23 11:34:49 +0100

    Update Oppg2.py

--- Oppg2.py
+++ Oppg2.py
@@ -1,1 +1,30 @@
 #Ny pythonfil
+from threading import Thread
+
+times = 1000000;
+i = 0
+
+def Inkrementer():
+  global i
+  for _ in range(times):
+    i += 1
+  
+def Dekrementer():
+  global i
+  for _ in range(times):
+    i -= 1
+
+
+
+def main():
+  someThread = Thread(target = Inkrementer, args = (),)
+  someThread2 = Thread(target = Dekrementer, args = (),)
+  someThread.start()
+  someThread2.start()
+  someThread.join()
+  someThread2.join()
+  print(i)
+
+
+
+main()
